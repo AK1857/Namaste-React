1b031b4bc3046f2ed75d923dafe71094955e9652
Ashish Kumar <ashish@okindia.com>
2023-09-27 22:12:46 +0530

    episoe 1 code

---
+++ Episode-1/app.js
@@ -0,0 +1,7 @@
+
+    let header=React.createElement("h1",{id:"header"},"Hello world from React");
+
+    let root=ReactDOM.createRoot(document.getElementById("root1"));
+
+    root.render(header);
+
